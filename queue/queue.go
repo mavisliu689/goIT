708e5aac356ddb278a0e69d187be7ca8eaba1087
mavisliu689 <r03241304@ntu.edu.tw>
2019-02-12 17:09:51 +0800

    update

--- queue/queue.go
+++ queue/queue.go
@@ -1,18 +1,23 @@
 package queue
 
+//An FIFO queue.
 type Queue []interface{}
 
+//Pushes the element into the queue.
+// 	e.g. go.Push(123)
 func (q *Queue) Push(v interface{}) {
 	//在內部確認類型為int 若非int則會報錯
 	*q = append(*q, v.(int))
 }
 
+//Pops eleement from head.
 func (q *Queue) Pop() interface{} {
 	head := (*q)[0]
 	*q = (*q)[1:]
 	return head.(int)
 }
 
+//Returns if th queue is empty or not.
 func (q *Queue) IsEmpty() bool {
 	return len(*q) == 0
 }
